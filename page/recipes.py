ba8375a317485a78de2cc93ccdec9d49e27126d0
Mohammad Saim <78817243+thisissaim@users.noreply.github.com>
2023-08-08 12:03:25 +0530

    Update recipes.py

--- page/recipes.py
+++ page/recipes.py
@@ -1,6 +1,6 @@
 
 import openai
-openai.api_key = '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'
+openai.api_key = 'INSERT OPENAI API KEY'
 
 def get_recipe_suggestions(predicted_food_item, user_preferences, calorie_requirements):
     prompt = f"""Give me recipe suggestions for {predicted_food_item} that align with my dietary preferences: {user_preferences} and calorie requirements: {calorie_requirements}.
